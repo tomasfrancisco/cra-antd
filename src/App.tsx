1a7e2ab6f15bf84e369514ed02f90d0ee9342d4a
Tomás Francisco <tomas@lightbase.nl>
2019-01-24 18:17:10 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+
+import { Button } from "antd";
+import React, { Component } from "react";
+
+export class App extends Component {
+  render() {
+    return <Button type="primary">Antd Button</Button>;
+  }
+}
