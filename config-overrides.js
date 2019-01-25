f8b9ff6e8b5d24e8bef64db5360a2b5d214098ea
Tomás Francisco <tomas@lightbase.nl>
2019-01-25 11:53:26 +0100

    fix babel injector

--- config-overrides.js
+++ config-overrides.js
@@ -4,10 +4,10 @@ module.exports = override(
   fixBabelImports("import", {
     libraryName: "antd",
     libraryDirectory: "es",
-    style: "css"
+    style: true
   }),
   addLessLoader({
-    javascriptEnabled: true,
-    modifyVars: { "primary-color": "#1DA57A" }
+    modifyVars: { "@primary-color": "#1DA57A" },
+    javascriptEnabled: true
   })
 );
